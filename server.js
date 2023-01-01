49a24f6ed0b5e92ee692b299858e6f71e2baec85
Thanh-hiep-NT-0105 <nguyenhiep0456@gmail.com>
2023-01-01 20:23:34 +0700

    add file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+//const express = require('express');
+import express from 'express';
+const path = require('path');
 const app = express();
 const port = 3001
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 app.get('/about', (req, res) => {

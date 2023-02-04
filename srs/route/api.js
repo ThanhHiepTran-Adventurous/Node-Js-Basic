564f7f10aa6eac87c5999e33da996353308688a3
Thanh-hiep-NT-0105 <nguyenhiep0456@gmail.com>
2023-02-05 00:09:15 +0700

    add controller

--- srs/route/api.js
+++ srs/route/api.js
@@ -8,6 +8,11 @@ let router = express.Router();
 const initAPIRoute = (app) => {
     router.get('/users', APIController.getAllUsers); // method GET -> READ DATA 
 
+    router.post('/create-user', APIController.createNewUser);
+
+    router.put('/update-user', APIController.updateUser);
+
+    router.delete('/delete-user/:id', APIController.deleteUser);
 
     return app.use('/api/v1/', router)
 }
